6dc50e1bf0a9c44f335403b8f58b96b62e381436
yamini <yaminiagarwal09@gmail.com>
2018-10-15 17:56:18 +0530

    Added density plots for happiness and sadness factors

--- exploratoryAnalysis_1.R
+++ exploratoryAnalysis_1.R
@@ -22,7 +22,13 @@ covarianceMatrix
 # factors across sections of our dataset to find the variables which effect these factors the most and in 
 # the end effect the happiness / sadness of people.
 
-# The first ten columns show th demographics
+dev.off()
+par(mfrow=c(2,5),mar=c(2,2,2,2))
+for (factorV in happinessFactors){
+  hist(happinessSadness[[factorV]],breaks = c(0,1,2,3,4,5),freq = FALSE,col="#3399FF",main="",mgp=c(1,0,0),xlab=factorV)
+}
+
+# The first ten columns show the demographics
 # As we studied in prior kernels,considering bmi instead if height and weight as separate metrics is better.
 # We have established that the height is in cms and weight in kgs.
 
